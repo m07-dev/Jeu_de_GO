550e11e619a7153bc19db50ad9c1cee8252ed315
Mohamed <fofanaa.mohamed45@gmail.com>
2025-04-26 23:28:43 +0200

    ajouts de fonctions

--- Plateau.java
+++ Plateau.java
@@ -1,6 +1,6 @@
 import javax.swing.*;
 
-public class Plateau {    // Création du plateau de Jeu
+public class Plateau { // Création du plateau de Jeu
 
     int taille;
     char[][] plateau;
@@ -16,11 +16,11 @@ public class Plateau {    // Création du plateau de Jeu
         }
     }
 
-    // Fonction qui affiche le plateau de Jeu 
+    // Fonction qui affiche le plateau de Jeu
     public void afficherPlateau() {
         for (int i = 0; i < plateau.length; i++) {
             for (int j = 0; j < plateau[i].length; j++) {
-                System.out.print(plateau[i][j]); 
+                System.out.print(plateau[i][j]);
             }
             System.out.println();
         }
@@ -28,17 +28,26 @@ public class Plateau {    // Création du plateau de Jeu
 
     // Fontion qui vérifie si on est en dehors du plateau
     public boolean enDehorsDesLimites(int posX, int posY) {
-        if(posX < 0 || posX >= taille || posY < 0 || posY >= taille) {
+        if (posX < 0 || posX >= this.taille || posY < 0 || posY >= this.taille) {
             return false;
         }
         return true;
     }
 
-    public boolean peutPoserPierre(int posX, int posY){
-        if(enDehorsDesLimites(posX, posY) && (plateau[posX][posY]== '·')){
+    public boolean peutPoserPierre(int posX, int posY) {
+        if (enDehorsDesLimites(posX, posY) && (plateau[posX][posY] == '·')) {
             return true;
         }
         return false;
+    }
+
+    // Fonction qui poser la pierre
+    public void poserPierre(int posX, int posY, char pierre) {
+        if (peutPoserPierre(posX, posY)) {
+            plateau[posX][posY] = pierre; // Il faudra changer en fonction du joueur sa pierre
+        } else {
+            JOptionPane.showMessageDialog(null,"Vous ne pouvez pas poser de pierre ici !, veuillez choisir un autre endroit","Attention", JOptionPane.WARNING_MESSAGE);
+        }
     }
 
 }
