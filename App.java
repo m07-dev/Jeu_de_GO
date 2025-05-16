4017b0166f060c42e1b0c9c247f13067a1fedc54
Mohamed <fofanaa.mohamed45@gmail.com>
2025-05-16 19:01:50 +0200

    Utilisation de GroupePierre et Comptage de Points

--- App.java
+++ App.java
@@ -4,31 +4,48 @@ import mypackage.Plateau;
 
 public class App {
     public static void main(String[] args) {
-        Plateau Goban = new Plateau(10);
-        Arena Arene = new Arena('w', 'b', 2, 0, 0);
-        boolean finDeJeu = true;
+        Plateau goban = new Plateau(10);
+        Arena arene = new Arena('w', 'b', 1, 0.5, 0);
+        boolean finDeJeu = false;
 
-        while (finDeJeu) { 
-        int[] coup = Arene.demanderCoup();
-        if(Arene.nbr_passerTour == 2){
-            JOptionPane.showMessageDialog(null,"Le Jeu est Terminé, Vous avez tous les deux passé vos Tour","FIN DU JEU", JOptionPane.ERROR_MESSAGE);
-            finDeJeu = false;
-        }
-        if (coup != null) {
-            int x = coup[0];
-            int y = coup[1];
-            char pierre = (Arene.joueurActuel == 1) ? Arene.pierreBlanc : Arene.pierreNoir;
-            if (Goban.peutPoserPierre(x, y)) {
-                Goban.poserPierre(x, y, pierre);
-                Goban.afficherPlateau();
-                Arene.changerJoueur();
-            } else {
-                JOptionPane.showMessageDialog(null,"Vous ne pouvez pas poser de pierre ici ! Veuillez choisir un autre endroit","Attention", JOptionPane.ERROR_MESSAGE);
+        while (!finDeJeu) {
+            int[] coup = arene.demanderCoup();
+
+            if (arene.nbr_passerTour == 2) {
+                JOptionPane.showMessageDialog(
+                    null,
+                    "Le Jeu est Terminé, vous avez tous les deux passé votre tour",
+                    "FIN DU JEU",
+                    JOptionPane.INFORMATION_MESSAGE
+                );
+                finDeJeu = true;
+                continue;
             }
-        } else {
-            System.out.println("Le joueur " + Arene.joueurActuel + " a passé son tour.");
-        }
-    }
 
+            if (coup != null) {
+                int x = coup[0];
+                int y = coup[1];
+                char pierre = (arene.joueurActuel == 1) ? arene.pierreBlanc : arene.pierreNoir;
+                if (goban.peutPoserPierre(x, y,goban.getPlateau())) {
+                    goban.poserPierre(x, y, pierre,goban.getPlateau());
+                    boolean[][] caseVisitee = new boolean[Plateau.taille][Plateau.taille];
+                    arene.groupePierre(x, y, goban.getPlateau(), caseVisitee, arene.joueurActuel);
+                    goban.afficherPlateau();
+
+                    System.out.println("Points Blanc : " + arene.pointBlanc + " Points Noir : " + arene.pointNoir);
+
+                    arene.changerJoueur();
+                } else {
+                    JOptionPane.showMessageDialog(
+                        null,
+                        "Vous ne pouvez pas poser de pierre ici ! Veuillez choisir un autre endroit.",
+                        "Erreur",
+                        JOptionPane.ERROR_MESSAGE
+                    );
+                }
+            } else {
+                System.out.println("Le joueur " + arene.joueurActuel + " a passé son tour.");
+            }
+        }
     }
 }
