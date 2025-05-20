fbd3e25a5d0bb19034cc5b422b58287cbe80c299
Mohamed <fofanaa.mohamed45@gmail.com>
2025-05-21 00:09:32 +0200

    Menu pour soit jouer avec l'IA ou entre humain

---
+++ Menu.java
@@ -0,0 +1,26 @@
+
+
+
+import javax.swing.JOptionPane;
+
+public class Menu {
+    // 1 = Humain vs Humain
+    // 2 = IA vs Humain
+    public static int afficherMenu() {
+        String[] options = { "Humain vs Humain", "IA vs Humain" };
+        int choix = JOptionPane.showOptionDialog(
+            null,
+            "Choisissez un mode de jeu :",
+            "Menu",
+            JOptionPane.DEFAULT_OPTION,
+            JOptionPane.INFORMATION_MESSAGE,
+            null,
+            options,
+            options[0]
+        );
+
+        // choix == 0 → Humain vs Humain
+        // choix == 1 → IA vs Humain
+        return (choix == 1) ? 2 : 1;
+    }
+}
